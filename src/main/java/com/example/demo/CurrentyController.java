9881a57b208e475df4f3a4f9461f5832118b0e72
Alex Castillo <gmacastil@gmail.com>
2018-05-12 22:36:14 -0500

    Ajuste de bug

--- src/main/java/com/example/demo/CurrentyController.java
+++ src/main/java/com/example/demo/CurrentyController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrentyController {
+	
+	private static final int MULTIPLO = 10;
 
 	@GetMapping("/currency-converter/quantity/{quantity}")
 	public CurrentyBean convertCurrency(@PathVariable BigDecimal quantity) {
-		BigDecimal bd = new BigDecimal(10);
+		BigDecimal bd = new BigDecimal(MULTIPLO);
 		return new CurrentyBean(quantity, quantity.multiply(bd));
 	}
 }
